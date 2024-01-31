7eb5d48d8ced3e3d9f5720d5b7deca94731b3fa3
takashiono2 <takashiono710@gmail.com>
2024-01-31 21:40:33 +0900

    section3_23

---
+++ 03-solid-principle/interfaceSegregation/goodExample.ts
@@ -0,0 +1,58 @@
+export {}
+
+interface Vehicle {
+    name: string;
+    color: string;
+}
+
+interface Movable {
+    start();
+    stop();
+}
+
+interface Flyable {
+    fly();
+}
+
+
+class Airplane implements Vehicle,Movable,Flyable {
+    constructor(
+        public name: string,
+        public color: string
+    ) {
+
+    }
+    start() {
+        console.log("start!");
+    }
+    stop() {
+        console.log("stop!");
+    }
+    fly() {
+        console.log("fly!");
+    }
+}
+
+class Car implements Movable,Vehicle {
+    constructor(
+        public name: string,
+        public color: string
+    ) {}
+
+    start() {
+        console.log("start!");
+    }
+    stop() {
+        console.log("stop!");
+    }
+}
+
+function run() {
+    const v1 = new Airplane("AirBus", "white");
+    const v2 = new Car("Prius", "black");
+
+    v1.fly();
+    v2.start();
+}
+
+run();
